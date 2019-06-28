209b07ded5d18afe6adb04e5a02d463ae4ba9a81
Qu <Quanvle@live.dk>
2019-06-28 10:56:07 +0200

    Task: Format number with comma separating thousands
    created a FornamNum method

--- Edabit/Code.cs
+++ Edabit/Code.cs
@@ -6,5 +6,16 @@ namespace Edabit
 {
     class Code
     {
+
+        ///<summary>
+        ///Difficulty: Medium
+        ///</summary>
+
+        // Format Number with Comma(s) Separating Thousands - https://edabit.com/challenge/GvGSPC9wiY4bS9AMg
+        public static string FormatNum(int num)
+        {
+            return string.Format("{0:N0}", num);
+        }
+
     }
 }
