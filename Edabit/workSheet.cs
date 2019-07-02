5f6c3a475955daddd03121075451a12af4581bdc
Qu <Quanvle@live.dk>
2019-07-02 15:53:52 +0200

    Created a new class to work edabit code with

---
+++ Edabit/workSheet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Edabit
+{
+    class workSheet
+    {
+
+        public static bool TimeForMilkAndCookies(int year, int month, int day)
+        {
+
+            return month == 12 && day == 24 ? true : false;
+        }
+    }
+}
